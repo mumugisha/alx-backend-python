daa38ee2e8a40e9d5bbe7429fc7cd5a73109ef33
mumugisha <prudent.nkubito@gmail.com>
2024-10-10 22:52:58 +0300

    Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Type-annotated function concat that takes two strings
+and returns their concatenation.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Arguments:
+    - str1: The first string
+    - str2: The second string
+    
+    Returns:
+    A concatenated string (str1 + str2)
+    """
+    return str1 + str2
