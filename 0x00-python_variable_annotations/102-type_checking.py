e263a35b6f67aa16dd0e336f26d84203ff07c4ec
mumugisha <prudent.nkubito@gmail.com>
2024-10-11 01:49:00 +0300

    Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Use mypy to validate the following piece of code
+"""
+from typing import List, Tuple
+
+
+def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+    """
+    Apply any necessary changes
+    """
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
