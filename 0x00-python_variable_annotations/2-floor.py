816381302870241d6ff7dff53039ee7af8c27b92
mumugisha <prudent.nkubito@gmail.com>
2024-10-10 23:15:53 +0300

    Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Write type-annotated function floor
+which takes a float n
+"""
+
+
+def floor(n: float) -> int:
+    """
+    argument and returns the floor of the float
+    """
+
+    return int(n)
