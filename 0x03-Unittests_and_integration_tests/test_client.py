932119a6b2b72968e47bde155c929cfa854a3857
mumugisha <prudent.nkubito@gmail.com>
2024-11-03 20:25:43 +0200

    Unittests and Integration Tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -67,9 +67,40 @@ class TestGithubOrgClient(unittest.TestCase):
         repository names based on a mocked response.
         """
         test_payload = [
-            {"name": "episodes.dart"},
-            {"name": "kratu"},
-        ]
+            {
+                "id": 7697149,
+                "name": "episodes.dart"
+                "private" False,
+                "owner": {
+                    "login": "google",
+                    "id": 1342004,
+            },
+            "fork": False,
+            "url": "https://api.github.com/repos/google/episodes.dart",
+            "created_at": "2013-01-19T00:31:37Z",
+            "updated_at": "2019-09-23T11:53:58Z",
+            "has_issues": True,
+            "forks": 22,
+            "default_branch": "master",
+            },
+            {
+                "id": 8566972,
+                "name": "kratu",
+                "private": False,
+                "owner": {
+                     "login": "google",
+                     "id": 1342004,
+                },
+                "fork": False,
+                "url": "https://api.github.com/repos/google/kratu",
+                "created_at": "2013-03-04T22:52:33Z",
+                "updated_at": "2019-11-15T22:22:16Z",
+                "has_issues": True,
+                "forks": 22,
+                "default_branch": "master",
+                },
+            ]
+        }
         mock_get_json.return_value = test_payload
 
         with patch(
